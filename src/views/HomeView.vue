64e6118839c8a2f13cf41f44aaf34b64aa5ea716
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-05-19 19:03:59 +0200

    :fire: Remove not existing components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
+<script>
+import { defineComponent} from 'vue'
+
+export default defineComponent({
+  name: 'HomeView'
+})
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style scoped lang="scss">
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+</style>
